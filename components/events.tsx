f5f315e4ac4dfa69eaf7da40b86af84616f26e55
Filippo Fontana <filippo@embrio.tech>
2025-01-08 11:29:23 +0100

    feat: handle past events

--- components/events.tsx
+++ components/events.tsx
@@ -24,8 +24,14 @@ export const Events: React.FC = (props) => {
 
   const cards = events.map((event) => {
     const eventDate = new Date(Date.parse(event.date_from));
+    const isPast = eventDate < new Date();
     return (
-      <Card key={event.slug} className="py-4 sm:flex-row" fullWidth={false} isBlurred>
+      <Card
+        key={event.slug}
+        className="py-4 sm:flex-row"
+        fullWidth={false}
+        isBlurred
+      >
         <CardHeader className="pb-0 pt-2 px-4 sm:w-auto flex-col items-start">
           <h4 className="font-bold text-large">{event.name.en}</h4>
           <p className="text-tiny uppercase font-bold">{event.location.en}</p>
@@ -36,16 +42,18 @@ export const Events: React.FC = (props) => {
         <CardFooter className="overflow-visible py-2 items-center justify-end">
           <Button
             className="w-full sm:w-auto"
-            color={event.testmode ? "warning" : "success"}
+            color={event.testmode || isPast ? "warning" : "success"}
             size="sm"
             radius="full"
-            variant={event.testmode ? "faded" : "solid"}
+            variant={event.testmode || isPast ? "faded" : "solid"}
             as={Link}
-            isDisabled={event.testmode}
+            isDisabled={event.testmode || isPast}
             href={event.public_url}
             isExternal={true}
           >
-            {event.testmode ? "Tickets Available Soon" : "Join Us!"}
+            {event.testmode && "Available Soon"}
+            {!event.testmode && !isPast && "Join Us!"}
+            {isPast && "Event Passed"}
           </Button>
         </CardFooter>
       </Card>
